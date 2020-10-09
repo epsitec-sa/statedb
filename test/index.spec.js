dd77dd76b57746d23647b2e8752ec536a9df53dc
Mathieu Schroeter <mathieu@schroetersa.ch>
2020-10-09 11:01:09 +0200

    Add xcraft-dev-rules dev dep

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,78 +1,77 @@
 'use strict';
 
-const fs       = require ('fs');
-const path     = require ('path');
-const {expect} = require ('chai');
-const StateDb  = require ('../lib/index');
+const fs = require('fs');
+const path = require('path');
+const {expect} = require('chai');
+const StateDb = require('../lib/index');
 
-const output = path.join (__dirname, './test');
+const output = path.join(__dirname, './test');
 
-describe ('index: new state', function () {
+describe('index: new state', function () {
   let stateDb = null;
 
-  beforeEach (function () {
-    stateDb = new StateDb (output, 'myApp');
+  beforeEach(function () {
+    stateDb = new StateDb(output, 'myApp');
   });
 
-  afterEach (function () {
-    fs.unlinkSync (`${output}.db`);
+  afterEach(function () {
+    fs.unlinkSync(`${output}.db`);
   });
 
-  it ('save and load state', function () {
-    stateDb.saveState ('myState1', {foo: 'foo'});
-    stateDb.saveState ('myState2', {bar: 'bar'});
+  it('save and load state', function () {
+    stateDb.saveState('myState1', {foo: 'foo'});
+    stateDb.saveState('myState2', {bar: 'bar'});
 
-    const state1 = stateDb.loadState ('myState1');
-    expect (state1).to.be.eql ({foo: 'foo'});
-    const state2 = stateDb.loadState ('myState2');
-    expect (state2).to.be.eql ({bar: 'bar'});
+    const state1 = stateDb.loadState('myState1');
+    expect(state1).to.be.eql({foo: 'foo'});
+    const state2 = stateDb.loadState('myState2');
+    expect(state2).to.be.eql({bar: 'bar'});
   });
 });
 
-describe ('index: existing state', function () {
-  it ('current state', function () {
-    const stateDb = new StateDb (path.join (__dirname, './sample'), 'myApp');
-    const state = stateDb.loadState ('myState');
-    expect (state).to.be.eql ({foo: 'bar'});
+describe('index: existing state', function () {
+  it('current state', function () {
+    const stateDb = new StateDb(path.join(__dirname, './sample'), 'myApp');
+    const state = stateDb.loadState('myState');
+    expect(state).to.be.eql({foo: 'bar'});
   });
 });
 
-describe ('index: wait for async writes', function () {
+describe('index: wait for async writes', function () {
   let stateDb = null;
 
-  before (function () {
-    stateDb = new StateDb (output, 'myApp', {
-      async: true
+  before(function () {
+    stateDb = new StateDb(output, 'myApp', {
+      async: true,
     });
   });
 
-  after (function () {
-    fs.unlinkSync (`${output}.db`);
+  after(function () {
+    fs.unlinkSync(`${output}.db`);
   });
 
-  it ('has 1000 pending writes', function () {
+  it('has 1000 pending writes', function () {
     for (let i = 0; i < 1000; i++) {
-      stateDb.saveState ('myState3', {[i]: i});
+      stateDb.saveState('myState3', {[i]: i});
     }
-    expect (stateDb.pendingWrites).to.be.eql (1000);
+    expect(stateDb.pendingWrites).to.be.eql(1000);
   });
 
-  it ('wait and flush pending writes', function (done) {
-    stateDb.waitForWrites (() => {
-      expect (stateDb.pendingWrites).to.be.eql (0);
-      done ();
+  it('wait and flush pending writes', function (done) {
+    stateDb.waitForWrites(() => {
+      expect(stateDb.pendingWrites).to.be.eql(0);
+      done();
     });
   });
 
-  it ('is consistent after wait', function () {
-    const state = stateDb.loadState ('myState3');
-    expect (state[999]).to.be.eql (999);
+  it('is consistent after wait', function () {
+    const state = stateDb.loadState('myState3');
+    expect(state[999]).to.be.eql(999);
   });
 
-  it ('saveState () return a promise', function () {
-    const promise = stateDb.saveState ('myState4', {foo: 'bar'});
-    expect (typeof promise.then).to.be.eql ('function');
-    return promise.then ();
+  it('saveState () return a promise', function () {
+    const promise = stateDb.saveState('myState4', {foo: 'bar'});
+    expect(typeof promise.then).to.be.eql('function');
+    return promise.then();
   });
-
 });
--- test/storage.spec.js
+++ test/storage.spec.js
@@ -1,64 +1,67 @@
 'use strict';
 
-const fs       = require ('fs');
-const path     = require ('path');
-const {expect} = require ('chai');
-const storage  = require ('../lib/storage');
-const output = path.join (__dirname, './test');
+const fs = require('fs');
+const path = require('path');
+const {expect} = require('chai');
+const storage = require('../lib/storage');
+const output = path.join(__dirname, './test');
 
-describe ('storage', function () {
+describe('storage', function () {
   let state = null;
 
-  beforeEach (function () {
-
-    state = storage (output, 'theKey');
-    state.initDb ({});
+  beforeEach(function () {
+    state = storage(output, 'theKey');
+    state.initDb({});
   });
 
-  afterEach (function () {
-    fs.unlinkSync (`${output}.db`);
+  afterEach(function () {
+    fs.unlinkSync(`${output}.db`);
   });
 
-  it ('state 1', function () {
-    state.persistState ('foo', {foo: 'bar'});
-    expect (state.getPersistedState ('foo')).to.be.eql ({foo: 'bar'});
+  it('state 1', function () {
+    state.persistState('foo', {foo: 'bar'});
+    expect(state.getPersistedState('foo')).to.be.eql({foo: 'bar'});
   });
 
-  it ('state 2', function () {
-    state.persistState ('bar', {bar: 'foo'});
-    expect (state.getPersistedState ('bar')).to.be.eql ({bar: 'foo'});
+  it('state 2', function () {
+    state.persistState('bar', {bar: 'foo'});
+    expect(state.getPersistedState('bar')).to.be.eql({bar: 'foo'});
   });
 });
 
-
-describe ('async storage', function () {
+describe('async storage', function () {
   let state = null;
 
-  beforeEach (function () {
-    state = storage (output, 'theKey', {
-      async: true
+  beforeEach(function () {
+    state = storage(output, 'theKey', {
+      async: true,
     });
-    state.initDb ({});
+    state.initDb({});
   });
 
-  afterEach (function () {
-    fs.unlinkSync (`${output}.db`);
+  afterEach(function () {
+    fs.unlinkSync(`${output}.db`);
   });
 
-  it ('persistState () return a promise', function () {
-    const promise = state.persistState ('yo', {yo: 'ba'});
-    expect (typeof promise.then).to.be.eql ('function');
-    return promise.then ();
+  it('persistState () return a promise', function () {
+    const promise = state.persistState('yo', {yo: 'ba'});
+    expect(typeof promise.then).to.be.eql('function');
+    return promise.then();
   });
 
-  it ('state 1', function () {
-    return state.persistState ('foo', {foo: 'bar'})
-      .then (() => expect (state.getPersistedState ('foo')).to.be.eql ({foo: 'bar'}));
+  it('state 1', function () {
+    return state
+      .persistState('foo', {foo: 'bar'})
+      .then(() =>
+        expect(state.getPersistedState('foo')).to.be.eql({foo: 'bar'})
+      );
   });
 
-  it ('state 2', function () {
-    return state.persistState ('bar', {bar: 'foo'})
-      .then (() => expect (state.getPersistedState ('bar')).to.be.eql ({bar: 'foo'}));
+  it('state 2', function () {
+    return state
+      .persistState('bar', {bar: 'foo'})
+      .then(() =>
+        expect(state.getPersistedState('bar')).to.be.eql({bar: 'foo'})
+      );
   });
-
 });
