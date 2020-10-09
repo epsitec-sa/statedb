5a549fcc856084bb99b5f441021a59466e90d2f3
Mathieu Schroeter <mathieu@schroetersa.ch>
2020-10-09 13:24:29 +0200

    Add a test with two apps

--- test/index.spec.js
+++ test/index.spec.js
@@ -29,6 +29,30 @@ describe('index: new state', function () {
   });
 });
 
+describe('index: new two states', function () {
+  let stateDb1 = null;
+  let stateDb2 = null;
+
+  beforeEach(function () {
+    stateDb1 = new StateDb(output, 'myApp1');
+    stateDb2 = new StateDb(output, 'myApp2');
+  });
+
+  afterEach(function () {
+    fs.unlinkSync(`${output}.db`);
+  });
+
+  it('save and load state', function () {
+    stateDb1.saveState('myState1', {foo: 'foo'});
+    stateDb2.saveState('myState2', {bar: 'bar'});
+
+    const state1 = stateDb1.loadState('myState1');
+    expect(state1).to.be.eql({foo: 'foo'});
+    const state2 = stateDb2.loadState('myState2');
+    expect(state2).to.be.eql({bar: 'bar'});
+  });
+});
+
 describe('index: existing state', function () {
   it('current state myApp', function () {
     const stateDb = new StateDb(path.join(__dirname, './sample'), 'myApp');
